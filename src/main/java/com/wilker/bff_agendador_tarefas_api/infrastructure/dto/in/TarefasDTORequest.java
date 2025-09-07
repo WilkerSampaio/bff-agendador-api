0847c0e4eabe3b91596ed22d46c8d2719837802e
Wilker <neoxextreme@gmail.com>
2025-09-07 00:32:21 -0400

    adicionado dtos e enum

---
+++ src/main/java/com/wilker/bff_agendador_tarefas_api/infrastructure/dto/in/TarefasDTORequest.java
@@ -0,0 +1,22 @@
+package com.wilker.bff_agendador_tarefas_api.infrastructure.dto.in;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TarefasDTORequest {
+
+
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+
+
+}
