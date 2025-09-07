5ad334deea1b89401c367cb58a662c0dee9c4a86
Wilker <neoxextreme@gmail.com>
2025-09-07 00:26:10 -0400

    primeiro commit

---
+++ src/main/java/com/wilker/bff_agendador_tarefas_api/BffAgendadorTarefasApiApplication.java
@@ -0,0 +1,13 @@
+package com.wilker.bff_agendador_tarefas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilker/bff_agendador_tarefas_api/BffAgendadorTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilker.bff_agendador_tarefas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
